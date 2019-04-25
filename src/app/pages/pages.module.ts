1796e96c2730ae9f6c0188ee9413ea9d52574c47
el gorrión <abcdise@gmail.com>
2019-04-25 12:38:10 -0300

    Sección 8 terminada

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,11 +8,13 @@ import { PAGES_ROUTES } from './pages.routes';
 import { FormsModule } from '@angular/forms';
 import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from '../pages/rxjs/rxjs.component';
 
 // ng2 charts
 
 import { ChartsModule } from 'ng2-charts';
-import { AccountSettingsComponent } from './account-settings/account-settings.component';
 
 // Temporal
 
@@ -25,7 +27,9 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
         PagesComponent,
         IncrementadorComponent,
         GraficoDonaComponent,
-        AccountSettingsComponent
+        AccountSettingsComponent,
+        PromesasComponent,
+        RxjsComponent
     ],
     exports : [
         DashboardComponent,
