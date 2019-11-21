5a1b04f3252a5d7d16bb264dd162f169293fc4d3
ricardo toc toc <ricardo.castillo@toctocviajes.com>
2019-11-21 16:02:15 -0300

    Sección 13 terminada

---
+++ src/app/config/config.ts
@@ -0,0 +1,2 @@
+// tslint:disable-next-line:eofline
+export const URL_SERVICIOS = "localhost:3000";
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from 'src/app/services/service.index';
+import { UsuarioService } from '../../services/usuario/usuario.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,7 +9,9 @@ import { SidebarService } from 'src/app/services/service.index';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor( public _sidebar: SidebarService) { }
+  constructor( 
+    public _sidebar: SidebarService,
+    public _usuarioService: UsuarioService) { }
 
   ngOnInit() {
   }
